b541de8b68a30f72f473adf3d1a9e939535c1b75
yashicaprasad <yashica.prasad@gmail.com>
2022-10-23 17:02:23 -0700

    Add files via upload

---
+++ Currency.java
@@ -0,0 +1,52 @@
+
+public abstract class Currency {
+	//Class Variables
+	protected int currencyWhole;
+	protected int currencyFrac;
+	
+	//Getter Setters
+	public int getCurrencyWhole() {
+		return currencyWhole;
+	}
+	public void setCurrencyWhole(int currencyWhole) {
+		this.currencyWhole = currencyWhole;
+	}
+	public int getCurrencyFrac() {
+		return currencyFrac;
+	}
+	public void setCurrencyFrac(int currencyFrac) {
+		this.currencyFrac = currencyFrac;
+	}
+	
+	//Default Construction
+	public Currency(){
+		
+	}
+	//Construction with type double as input
+	public Currency(double amt){
+		String doubleAsString = String.valueOf(amt);
+		int indexOfDecimal = doubleAsString.indexOf(".");
+		currencyWhole = Integer.parseInt(doubleAsString.substring(0, indexOfDecimal));
+		currencyFrac = Integer.parseInt(doubleAsString.substring(indexOfDecimal));
+	}
+	//Copy Constructor
+	public Currency(Currency c){
+		currencyWhole = c.currencyWhole;
+		currencyFrac =  c.currencyFrac;
+	}
+	
+	public abstract void add();
+	
+	public abstract void subtract();
+	
+	public abstract Boolean isEqual();
+	
+	public abstract Boolean isGreater();
+	
+	public abstract void print();
+	
+	//Destructor
+	protected void finalize(){  
+	
+	}  
+}
---
+++ Dollar.java
@@ -0,0 +1,84 @@
+
+public class Dollar extends Currency{
+	Dollar(){
+		super();
+	}
+	private String currencyType = "Dollar";
+	
+	public void add(Currency c) {		
+		if(c.currencyWhole > 0) {
+			currencyWhole += c.currencyWhole;
+			//convert all calculations to cents
+			int netCents = currencyWhole*100 + currencyFrac + c.currencyFrac;
+			currencyWhole = netCents/100;
+			currencyFrac = netCents%100;
+		}
+		else {
+			
+		}
+	}
+	
+	public void subtract(Currency c) {
+		if(c.currencyWhole > 0 && !isGreater(c)) {
+			currencyWhole -= c.currencyWhole;
+			//convert all calculations to cents
+			int netCents = currencyWhole*100 + currencyFrac + c.currencyFrac;
+			currencyWhole = netCents/100;
+			currencyFrac = netCents%100;
+		}
+		else {
+			
+		}
+	}
+	public Boolean isEqual(Currency c) {
+		if(currencyWhole==c.currencyWhole && currencyFrac==c.currencyFrac) {
+			return true;
+		}
+		return false;
+	}
+	public Boolean isGreater(Currency c) {
+		if(currencyWhole == c.currencyWhole) {
+			if(currencyFrac<c.currencyFrac)
+				return true;
+			else return false;
+		}
+		else if(currencyWhole < c.currencyWhole) {
+			return true;
+		}
+		return false;
+	}
+	public void print(Currency c) {	
+		System.out.println(c.currencyWhole + "." + c.currencyFrac);
+	}
+
+	@Override
+	public void add() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void subtract() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Boolean isEqual() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean isGreater() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
