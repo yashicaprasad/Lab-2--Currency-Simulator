cad04bb9fab3cad8e84ce9f465a4f82f11bc74d3
yashicaprasad <yashica.prasad@gmail.com>
2022-10-24 20:55:46 -0700

    Update Pound.java

--- Pound.java
+++ Pound.java
@@ -1,4 +1,8 @@
-
+/*
+Lab 2
+Yashica Prasad and Lois Wong
+Purpose of the assignment: Demonstrate use of classes, polymorphism, and inheritance
+ */
 public class Pound extends Currency{
 	public Pound(){
 		super();
