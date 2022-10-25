ef194b65231c168acb025138588557637a502651
yashicaprasad <yashica.prasad@gmail.com>
2022-10-24 20:56:06 -0700

    Update Dollar.java

--- Dollar.java
+++ Dollar.java
@@ -1,4 +1,8 @@
-
+/*
+Lab 2
+Yashica Prasad and Lois Wong
+Purpose of the assignment: Demonstrate use of classes, polymorphism, and inheritance
+ */
 public class Dollar extends Currency{
 	public Dollar(){
 		super();
