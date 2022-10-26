89c696bf5a0aa6f1636731bbe795d43360aefd85
Lois-Wong <Lois@lois-macbook-pro.local>
2022-10-26 12:11:01 -0700

    added input statement for Scanner, changed == to Object.equals(), changed Double.valueOf() to Double.parseDouble, changed while loop to for loop

--- Lab2Main.java
+++ Lab2Main.java
@@ -3,52 +3,63 @@ Lab 2
 Yashica Prasad and Lois Wong
 Purpose of the assignment: Demonstrate use of classes, polymorphism, and inheritance
  */
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Lab2Main {
-	public static void main() {
-		Currency[] baseCase;
-		baseCase = new Currency[2];
-		baseCase[0] = new Pound();
-		baseCase[1] = new Dollar();
+	public static void main(String[] args) {
+		Currency[] currencyArr = new Currency[2]; //array of references
+		currencyArr[0] = new Pound();
+		currencyArr[1] = new Dollar();
 
 		Dollar d1 = null;
 		Pound p1 = null;
-		Scanner myObj = new Scanner(System.in); 
-		String in = myObj.nextLine();
-		
-		String[] separatedInput = in.split(" ");
-		
-		
-		while(separatedInput[0]!="q") {
-			if(separatedInput[3]=="dollar") {
-				d1 = new Dollar(Double.valueOf(separatedInput[2]));
-				//debugging
-				System.out.println("Dollar obj created");
-			}
-			if(separatedInput[3]=="pound") {
-				p1 = new Pound(Double.valueOf(separatedInput[2]));
-				//debugging
-				System.out.println("Pound obj created");
-			}
 
-			if(separatedInput[1]=="p") {
-				if(separatedInput[0]=="a") {
-					p1.add();
-				}
-				if(separatedInput[0]=="s") {
-					p1.subtract();
-				}
+		for(int i = 0; i > -1; i++) {
+			Scanner myObj = new Scanner(System.in);
+			System.out.printf("Please enter a command: ");
+			String input = myObj.nextLine();
+			String[] separatedInput = input.split(" ");
+
+			if (Objects.equals(separatedInput[0], "q"))
+			{
+				i = -2;
 			}
-			if(separatedInput[1]=="d") {
-				if(separatedInput[0]=="a") {
-					d1.add();
-				}
-				if(separatedInput[0]=="s") {
-					d1.subtract();
+			else {
+					if (Objects.equals(separatedInput[3], "dollar"))
+					{
+						//debugging
+						System.out.println(Double.parseDouble(separatedInput[2]));
+						d1 = new Dollar(Double.parseDouble(separatedInput[2]));
+					}
+					if(Objects.equals(separatedInput[3], "pound"))
+					{
+						p1 = new Pound(Double.parseDouble(separatedInput[2]));
+					}
+					if(Objects.equals(separatedInput[1], "p"))
+					{
+						if (Objects.equals(separatedInput[0], "a"))
+						{
+							p1.add();
+						}
+						if(Objects.equals(separatedInput[0], "s"))
+						{
+							p1.subtract();
+						}
+					}
+					if(Objects.equals(separatedInput[1], "d"))
+					{
+						if(Objects.equals(separatedInput[0], "a"))
+						{
+							d1.add();
+						}
+						if(Objects.equals(separatedInput[0], "s"))
+						{
+							d1.subtract();
+						}
+					}
 				}
 			}
 		}
-	}
 }
 
