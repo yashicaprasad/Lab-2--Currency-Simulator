77a39ea7d3fa08151cd475bb41876055d84b2c72
Lois-Wong <Lois@lois-macbook-pro.local>
2022-11-04 06:27:15 -0700

    created dollar array and 3 objects

--- Lab3Main.java
+++ Lab3Main.java
@@ -1,14 +1,29 @@
 
 public class Lab3Main {
 	public static void main(String[] args) {
+		System.out.println("Welcome to Currency Simulator by Yashica Prasad and Lois Wong ");
 		Lab3Main testinstance= new Lab3Main();
-//		testinstance.testLinkedList();
-		
+
 		testinstance.testStack();
 		testinstance.testQueue();
 
 
+		Currency[] dollarArray = new Dollar[20];
+		dollarArray = new Currency[]{new Dollar(57.12), new Dollar(23.44),
+		new Dollar(87.43), new Dollar(68.99), new Dollar(111.22),
+				new Dollar(44.55), new Dollar(77.77), new Dollar(18.36),
+				new Dollar(543.21), new Dollar(20.21), new Dollar(345.67),
+				new Dollar(36.18), new Dollar(48.48), new Dollar(101.00),
+				new Dollar(11.00), new Dollar(21.00), new Dollar(51.00),
+				new Dollar(1.00), new Dollar(251.00), new Dollar(151.00)};
+
+		SinglyLinkedList singlyLinkedListObj = new SinglyLinkedList();
+		Stack stackObj = new Stack();
+		Queue queueObj = new Queue();
+
 	}
+
+
 	
 	public void testStack() {
 		Currency b = new Dollar(11.75);
