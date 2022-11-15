c66d556ad7014de487c6156ba99c322c9eccf0e2
Lois-Wong <Lois@lois-macbook-pro.local>
2022-11-15 11:44:53 -0800

    added breadth first traversal method

--- BST.java
+++ BST.java
@@ -6,7 +6,68 @@ public class BST {
     BST() { this.root = null; }
  
     BST(double value) { this.root = new BSTNode(new Dollar(value)); }
- 
+
+    /** breadth-first traversal
+     * @purpose process tree using breadth-first traversal
+     * @pre root is node to be processed
+     * @post tree is processed
+     * @return N/A
+
+     * @pseudocode
+     if(root == null)
+        print("root is null")
+        return
+
+     BSTNode currentNode = root
+     queueObj = new Queue()
+     while (currentNode != null)
+        queueObj.enQueue(currentNode.getData());
+        if(currentNode.getLeft() != null)
+            queueObj.enQueue(currentNode.getLeft().getData());
+        if(currentNode.getRight() != null)
+            queueObj.enQueue((currentNode.getRight().getData()));
+
+    while (!queueObj.isListEmpty())
+        Currency tempNode = queueObj.dequeue();
+    currentNode = null;
+     */
+    public void breadthFirstTraversal(BSTNode root)
+    {
+        //check if root is null
+        if(root == null)
+        {
+            System.out.println("Root is null");
+            return;
+        }
+
+        //fill the queue
+        BSTNode currentNode = root;
+        Queue queueObj = new Queue();
+        while(currentNode != null)
+        {
+            queueObj.enQueue(currentNode.getData());
+            if(currentNode.getLeft() != null)
+            {
+                queueObj.enQueue(currentNode.getLeft().getData());
+            }
+            if(currentNode.getRight() != null)
+            {
+                queueObj.enQueue((currentNode.getRight().getData()));
+            }
+        }
+
+        //process by dequeue
+        while (!queueObj.isListEmpty())
+        {
+            Currency tempNode = queueObj.dequeue();
+            tempNode.print(); // check
+        }
+        currentNode = null; //may be unnecessary
+
+        //destroy queue
+
+    }
+
     /* A recursive function to
        insert a new key in BST */
     BSTNode insertRec(BSTNode insert)
